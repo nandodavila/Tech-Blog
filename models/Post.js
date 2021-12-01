042f6a7f5bd08d0178a3bd70205ddd80c94dcc2f
Nando <nandodavila94@gmail.com>
2021-11-30 19:07:35 -0500

    mostly working, have to get comments working

--- models/Post.js
+++ models/Post.js
@@ -16,19 +16,24 @@ Post.init(
       allowNull: false,
     },
     content: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
       type: DataTypes.DATE,
       allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
     user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
+    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'post',
--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comments = require('./Comments');
 
-User.hasMany(Painting, {
-  foreignKey: 'gallery_id',
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-Painting.belongsTo(Gallery, {
-  foreignKey: 'gallery_id',
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
-module.exports = { User, Gallery, Painting };
+module.exports = { User, Post };
