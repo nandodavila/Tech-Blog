b88c741f0fbc9a5159e51da517af2bab3db3aea5
Nando <nandodavila94@gmail.com>
2021-12-02 20:03:27 -0500

    I think im close with getting comments to display, still think theres an issue with my handlebars syntax

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -11,6 +11,10 @@ router.get('/', async (req, res) => {
           model: User,
           attributes: ['username'],
         },
+        {
+          model: Comment,
+          attributes: ['comment']
+        }
       ],
     });
 
